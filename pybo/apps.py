7039cdeb11f793e3d3539a04321383dca368d627
fmfu51 <fmfu5129@gmail.com>
2021-12-18 17:15:07 +0900

    모델 데이터 생성 후 장고 셸로 질문, 답변 생성

--- pybo/apps.py
+++ pybo/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PyboConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pybo'
+    name = 'pybo'   # pybo 앱을 settings.py 에 추가하면 자동으로 생기는 코드들
--- pybo/models.py
+++ pybo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    subject = models.CharField(max_length=200)  # 글자 길이가 제한된 문자열은 CharField 타입 사용
+    content = models.TextField()    # TextField는 제한이 없는 문자열에 사용
+    create_date = models.DateTimeField()    # 날짜, 시간에 관계된 속성은 DateTimeField 사용
+
+    def __str__(self):  # 모델을 조회할 때, id 값 대신 제목을 표시하는 함수
+        return self.subject
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)  # ForeignKey는 다른 모델과 연결하며, on_delete~ 는 이 답변과 연결된 질문이 삭제되면 함께 삭제된다는 뜻
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
