ad965d45855c3ac7ab0cc2adc6af2aba838b8752
fmfu51 <fmfu5129@gmail.com>
2021-12-18 18:14:16 +0900

    질문목록과 질문목록상세페이지 생성

--- pybo/urls.py
+++ pybo/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [     # config/urls.py에 있던 매핑 여기로 옮기고 pybo관련 매핑은 앞으로도 여기서~
     path('', views.index),
+    path('<int:question_id>/', views.detail),   # <int:question_id> int 사용으로 숫자가 매핑됨, id에서 다 빼고 숫자만 남음
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,4 +1,13 @@
-from django.http import HttpResponse
+from .models import Question
+from django.shortcuts import render, get_object_or_404
 
-def index(request): # request : index함수의 매개변수
-    return HttpResponse("안녕하세요. pybo에 오신 것을 환영합니다.") # HttpResponse는 요청한 페이지에 응답을 할 때 사용하는 장고 클래스
+
+def index(request):     # pybo 목록 출력 (question_list)
+    question_list = Question.objects.order_by('-create_date')   # 질문 목록 데이터를 가져와 order_by(조회결과정렬)하는 함수, -create_date는 역방향 정렬
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)  # 파이썬 데이터를 템플릿에 적용하여 HTML로 반환하는 함수
+
+def detail(request, question_id):       # pybo 내용 출력 (question_detail)
+    question = get_object_or_404(Question, pk=question_id)  # pk는 Question 모델의 기본키(Primary Key)인 id를 의미
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
