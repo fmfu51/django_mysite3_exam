0af59eba76d34b453bf28e57936cf6e4fc6db02a
fmfu51 <fmfu5129@gmail.com>
2021-12-18 17:27:00 +0900

    장고관리자 생성 및 검색 기능 생성

--- pybo/admin.py
+++ pybo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Question, Answer
 
-# Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+class AnswerAdmin(admin.ModelAdmin):
+    search_fields = ['content']
+
+admin.site.register(Question, QuestionAdmin)   # admin 관리자에 Question 모델을 등록하면, 장고 관리자 화면에서 Question을 관리할 수 있다
+admin.site.register(Answer, AnswerAdmin)
