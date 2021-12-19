caa7477456a3c8434a45e8d546fd156cd5d02332
fmfu51 <fmfu5129@gmail.com>
2021-12-19 17:26:36 +0900

    질문 목록 페이징 처리

--- pybo/views.py
+++ pybo/views.py
@@ -3,12 +3,21 @@ from django.utils import timezone
 
 from .forms import QuestionForm, AnswerForm
 from .models import Question
+from django.core.paginator import Paginator
 
 
 def index(request):  # pybo 목록 출력 (question_list)
-    question_list = Question.objects.order_by(
-        '-create_date')  # 질문 목록 데이터를 가져와 order_by(조회결과정렬)하는 함수, -create_date는 역방향 정렬
-    context = {'question_list': question_list}
+    # 입력 파라미터
+    page = request.GET.get('page', '1')  # 페이지
+
+    # 조회
+    question_list = Question.objects.order_by('-create_date')
+
+    # 페이징처리
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)  # 파이썬 데이터를 템플릿에 적용하여 HTML로 반환하는 함수
 
 
