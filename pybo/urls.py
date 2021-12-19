2768876af4810890898c7ad2ace0a4c066dddbf8
fmfu51 <fmfu5129@gmail.com>
2021-12-19 18:58:35 +0900

    질문답변 수정, 삭제, 수정일시 추가

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from . import views
-app_name = 'pybo'   # 앱의 이름을 pybo로 하겠다는 뜻
 
-urlpatterns = [     # config/urls.py에 있던 매핑 여기로 옮기고 pybo관련 매핑은 앞으로도 여기서~
-    path('', views.index, name='index'),    # 질문목록매핑, name은 이 url 매핑의 별칭! 이름을 정해주는 것
-    path('<int:question_id>/', views.detail, name='detail'),   # 질문상세매핑, <int:question_id> int 사용으로 숫자가 매핑됨, id에서 다 빼고 숫자만 남음
-    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),    # 답변 등록 매핑
-    path('question/create/', views.question_create, name='question_create'),    # 질문 등록 매핑
+app_name = 'pybo'  # 앱의 이름을 pybo로 하겠다는 뜻
+
+urlpatterns = [  # config/urls.py에 있던 매핑 여기로 옮기고 pybo관련 매핑은 앞으로도 여기서~
+    path('', views.index, name='index'),  # 질문목록매핑, name은 이 url 매핑의 별칭! 이름을 정해주는 것
+    path('<int:question_id>/', views.detail, name='detail'),
+    # 질문상세매핑, <int:question_id> int 사용으로 숫자가 매핑됨, id에서 다 빼고 숫자만 남음
+    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),  # 답변 등록 매핑
+    path('question/create/', views.question_create, name='question_create'),  # 질문 등록 매핑
+    path('question/modify/<int:question_id>/', views.question_modify, name='question_modify'),  # 질문 수정 매핑
+    path('question/delete/<int:question_id>/', views.question_delete, name='question_delete'),  # 질문 삭제 매핑
+    path('answer/modify/<int:answer_id>/', views.answer_modify, name='answer_modify'),  # 답변 수정 매핑
+    path('answer/delete/<int:answer_id>/', views.answer_delete, name='answer_delete'),  # 답변 삭제 매핑
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,9 +1,10 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
 from .forms import QuestionForm, AnswerForm
-from .models import Question
+from .models import Question, Answer
 from django.core.paginator import Paginator
 
 
@@ -28,7 +29,7 @@ def detail(request, question_id):  # pybo 내용 출력 (question_detail)
     return render(request, 'pybo/question_detail.html', context)
 
 
-@login_required(login_url='common:login')   # @login_required 애너테이션이 붙은 함수는 로그인이 필요한 함수를 의미
+@login_required(login_url='common:login')  # @login_required 애너테이션이 붙은 함수는 로그인이 필요한 함수를 의미
 def answer_create(request, question_id):  # pybo 답변 등록
     question = get_object_or_404(Question, pk=question_id)
     if request.method == "POST":
@@ -49,14 +50,73 @@ def answer_create(request, question_id):  # pybo 답변 등록
 @login_required(login_url='common:login')
 def question_create(request):  # pybo 질문 등록
     if request.method == 'POST':
-        form = QuestionForm(request.POST)   # request.POST에는 화면에서 사용자가 입력한 내용들이 담겨있음
+        form = QuestionForm(request.POST)  # request.POST에는 화면에서 사용자가 입력한 내용들이 담겨있음
         if form.is_valid():
             question = form.save(commit=False)
             question.author = request.user  # 현재 로그인한 계정이 글쓴이가 되므로 requset.user로 작성
             question.create_date = timezone.now()
-            question.save() # 입력한 질문데이터를 저장, 시간도 저장 한 다음 index URL로 리턴된다!
-            return redirect('pybo:index')   # 빛의 속도로 저장한 다음 index 페이지를 띄우는 것
+            question.save()  # 입력한 질문데이터를 저장, 시간도 저장 한 다음 index URL로 리턴된다!
+            return redirect('pybo:index')  # 빛의 속도로 저장한 다음 index 페이지를 띄우는 것
     else:
         form = QuestionForm()
     context = {'form': form}
     return render(request, 'pybo/question_form.html', context)
+
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):  # 질문 수정
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('pybo:detail', question_id=question.id)
+
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.modify_date = timezone.now()  # 수정일시 저장
+            question.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = QuestionForm(instance=question)
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
+
+
+@login_required(login_url='common:login')
+def question_delete(request, question_id):  # 질문 삭제
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '삭제권한이 없습니다')
+        return redirect('pybo:detail', question_id=question.id)
+    question.delete()
+    return redirect('pybo:index')
+
+@login_required(login_url='common:login')
+def answer_modify(request, answer_id):  # 답변 수정
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('pybo:detail', question_id=answer.question.id)
+
+    if request.method == "POST":
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.modify_date = timezone.now()
+            answer.save()
+            return redirect('pybo:detail', question_id=answer.question.id)
+    else:
+        form = AnswerForm(instance=answer)
+    context = {'answer': answer, 'form': form}
+    return render(request, 'pybo/answer_form.html', context)
+
+
+@login_required(login_url='common:login')
+def answer_delete(request, answer_id):  # 답변 삭제
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '삭제권한이 없습니다')
+    else:
+        answer.delete()
+    return redirect('pybo:detail', question_id=answer.question.id)
