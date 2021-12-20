b3b15399219f83ffbb9fbca25237be97d6b49ec5
fmfu51 <fmfu5129@gmail.com>
2021-12-20 11:04:15 +0900

    추천 기능 생성

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from pybo import views
+from pybo.views import base_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),    # include : 직역하면 pybo.urls를 포함하겠다는 뜻의 함수
     path('common/', include('common.urls')),    # config의 url에 인클루드로 연결
-    path('', views.index, name='index'),  # '/' 에 해당되는 path
+    path('', base_views.index, name='index'),  # '/' 에 해당되는 path
 ]
