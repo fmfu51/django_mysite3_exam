8fa7c8c03cc62c8b66d29efe3b3e2ef1e71b96dc
fmfu51 <fmfu5129@gmail.com>
2021-12-20 13:27:08 +0900

    검색 및 정렬 기능 추가

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,21 +1,38 @@
 from django.core.paginator import Paginator
+from django.db.models import Q, Count
 from django.shortcuts import render, get_object_or_404
 from ..models import Question   # 하위 디렉토리로 옮겨서 경로가 한번 더 늘어났으니 .이 하나 더!
 
 
-def index(request):  # pybo 목록 출력 (question_list)
+def index(request): # 목록 출력
     # 입력 파라미터
     page = request.GET.get('page', '1')  # 페이지
+    kw = request.GET.get('kw', '')  # 검색어
+    so = request.GET.get('so', 'recent')  # 정렬기준
 
-    # 조회
-    question_list = Question.objects.order_by('-create_date')
+    # 정렬
+    if so == 'recommend':
+        question_list = Question.objects.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        question_list = Question.objects.annotate(num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+    else:  # recent
+        question_list = Question.objects.order_by('-create_date')
+
+    # 검색
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목검색
+            Q(content__icontains=kw) |  # 내용검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이검색
+        ).distinct()
 
     # 페이징처리
     paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj}
-    return render(request, 'pybo/question_list.html', context)  # 파이썬 데이터를 템플릿에 적용하여 HTML로 반환하는 함수
+    context = {'question_list': page_obj, 'page': page, 'kw': kw, 'so': so}
+    return render(request, 'pybo/question_list.html', context)
 
 
 def detail(request, question_id):  # pybo 내용 출력 (question_detail)
